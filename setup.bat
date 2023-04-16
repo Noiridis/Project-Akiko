4077dac4926fe9595e7bb50ee2696625ceb81943
Noiridis <130939057+Noiridis@users.noreply.github.com>
2023-04-17 01:25:05 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -3,4 +3,4 @@
 git pull
 
 start cmd /k "cd frontend && npm i && npx vite"
-start cmd /k "cd backend && pip install -r requirements.txt && python server.py"
+start cmd /k "cd backend && pip install -r full_requirements.txt && python server.py"
